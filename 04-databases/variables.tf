1bfd2336d2f4172fb243f022b6d5af26c7499827
MachaSreenath <sreenathshree9@gmail.com>
2025-01-24 21:33:21 +0530

    roboshop infra dev changes

---
+++ 04-databases/variables.tf
@@ -0,0 +1,19 @@
+variable "common_tags" {
+  default = {
+    Project = "roboshop"
+    Environment = "dev"
+    Terraform = "true"
+  }
+}
+
+variable "project_name" {
+  default = "roboshop2"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "forpractice.uno"
+}
