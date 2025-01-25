3444019c9c4bce2519e098a2a93d934f0bb4280d
MachaSreenath <sreenathshree9@gmail.com>
2025-01-25 12:31:00 +0530

    roboshop infra dev changes

--- 04-databases/main.tf
+++ 04-databases/main.tf
@@ -44,3 +44,51 @@ resource "null_resource" "mongodb" {
     ]
   }
 }
+
+module "redis" {
+    source  = "terraform-aws-modules/ec2-instance/aws"
+    ami = data.aws_ami.centos8.id
+    name = "${local.ec2_name}-redis"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [data.aws_ssm_parameter.redis_sg_id.value]
+    subnet_id = local.database_subnet_id
+    tags = merge(
+      var.common_tags,
+      {
+        Component = "redis"
+      },
+      {
+        Name = "${local.ec2_name}-redis"
+      }
+  )
+}
+
+resource "null_resource" "redis" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    instance_id = module.redis.id
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host = module.redis.private_ip
+    type = "ssh"
+    user = "centos"
+    password = "DevOps321"
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh redis dev"
+    ]
+  }
+}
+
