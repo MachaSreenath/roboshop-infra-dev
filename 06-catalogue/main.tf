1e3597957bb0ea620ea93d512b42628991d2e0a9
MachaSreenath <sreenathshree9@gmail.com>
2025-01-29 13:03:33 +0530

    roboshop infra dev changes

--- 06-catalogue/main.tf
+++ 06-catalogue/main.tf
@@ -13,3 +13,46 @@ resource "aws_lb_target_group" "catalogue" {
     matcher             = "200-299"
   }
 }
+
+module "catalogue" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami                    = data.aws_ami.centos8.id
+  name                   = "${local.name}-${var.tags.Component}-ami"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
+  subnet_id              = element(split(",", data.aws_ssm_parameter.private_subnet_ids.value), 0)
+  iam_instance_profile   = "iamroleforec2creation"
+  tags = merge(
+    var.common_tags,
+    var.tags
+  )
+}
+
+resource "null_resource" "catalogue" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    instance_id = module.catalogue.id
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host     = module.catalogue.private_ip
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh catalogue dev"
+    ]
+  }
+}
