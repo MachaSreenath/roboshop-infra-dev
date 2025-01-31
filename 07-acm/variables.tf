f7daf761f2e8f2aedbebc1901fbb74dbea5ae842
MachaSreenath <sreenathshree9@gmail.com>
2025-01-31 11:58:59 +0530

    roboshop infra dev changes

---
+++ 07-acm/variables.tf
@@ -0,0 +1,25 @@
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "tags" {
+  default = {
+    Component = "acm"
+  }
+}
+
+variable "project_name" {
+  default = "roboshop2"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "forpractice.uno"
+}
