1c2d8aab5ac845b929ea9d4d8c593a146a86de41
MachaSreenath <sreenathshree9@gmail.com>
2025-02-01 19:51:39 +0530

    roboshop infra dev changes

---
+++ 10-cdn/variables.tf
@@ -0,0 +1,25 @@
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "tags" {
+  default = {
+    Component = "cdn"
+  }
+}
+
+variable "project_name" {
+  default = "roboshop2"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "forpractice.uno"
+}
