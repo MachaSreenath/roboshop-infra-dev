28d8a4352c0d2ca81fc670fab09785eb779a264e
MachaSreenath <sreenathshree9@gmail.com>
2025-02-03 13:46:01 +0530

    roboshop infra dev changes

--- 11-user/data.tf
+++ 11-user/data.tf
@@ -9,3 +9,7 @@ data "aws_ssm_parameter" "user_sg_id" {
 data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/private_subnet_ids"
 }
+
+data "aws_ssm_parameter" "app_alb_listener_arn" {
+  name = "/${var.project_name}/${var.environment}/app_alb_listener_arn"
+}
